6c6dc5ddbc7a915cc894313e5ecacd6eed50ca4c
Jguinanzaca <Joshua.Guinanzaca16@myhunter.cuny.edu>
2025-03-25 11:35:34 -0400

    added BoxSizer, attempt to implement column of boxes next

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -5,4 +5,6 @@
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);
 	panel->SetBackgroundColour(wxColor(0,0,200));
+
+	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
 }
