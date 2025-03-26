0b51df5ba71b8ac6ee8559d31bf003bb61644d7e
Jguinanzaca <Joshua.Guinanzaca16@myhunter.cuny.edu>
2025-03-26 19:31:35 -0400

    added new event for switch (now removes and re-adds children); added status bar at bottom for testing purposes; changed signature of CreateButtons from wxPanel to wxWindow

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -2,18 +2,34 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 
+
+wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
+
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* firstPanel = new wxPanel(this, wxID_ANY, wxPoint(0, 0), wxSize(800,800));
 	wxPanel* secondPanel = new wxPanel(this, wxID_ANY, wxPoint(800, 0), wxSize(750, 800));
 	firstPanel->SetBackgroundColour(wxColor(0, 0, 200));
 	secondPanel->SetBackgroundColour(wxColor(0, 0, 200));
 
-	wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
 
 	CreateButtons(sizer, firstPanel);
+	CreateStatusBar();
 
 	wxButton* switchScreenButton = new wxButton(secondPanel, wxID_ANY, "Switch Screens", wxPoint(500, 700), wxSize(100, 50));
+	switchScreenButton->Bind(wxEVT_BUTTON, &MainFrame::SwitchButtonClicked, this);
 
+}
+
+void MainFrame::SwitchButtonClicked(wxCommandEvent& evt) {
+	
+	if (this->GetChildren()[0]->GetChildren().size() == 0) {
+		CreateButtons(sizer, this->GetChildren()[0]);
+		wxLogStatus("Children restored");
+	}
+	else {
+		this->GetChildren()[0]->DestroyChildren();
+		wxLogStatus("Children successfully destroyed");
+	}
 }
 
 
@@ -23,7 +39,7 @@ moves to the next column of buttons.
 
 Second loop then edits each inline button color
 */
-void MainFrame::CreateButtons(wxBoxSizer* sizer, wxPanel* panel) {
+void MainFrame::CreateButtons(wxBoxSizer* sizer, wxWindow* panel) {
 	int x = 150;
 	int y = 100;
 	for (int i = 0; i < 15; i++) {
--- MainFrame.h
+++ MainFrame.h
@@ -5,6 +5,7 @@ class MainFrame : public wxFrame { //mainframe inherits from wxFrame
 public:
 	MainFrame(const wxString& title); //wxstring is str implementation of wxwidgets, 
 	//whenever wxstring is needed pass char array or stl str
-	void CreateButtons(wxBoxSizer* sizer, wxPanel* panel);
+	void CreateButtons(wxBoxSizer* sizer, wxWindow* panel);
+	void SwitchButtonClicked(wxCommandEvent& evt);
 };
 
