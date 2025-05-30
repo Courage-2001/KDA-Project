de5b989881d0d4ae043e17b0af570d4d3de17f69
Jguinanzaca <Joshua.Guinanzaca16@myhunter.cuny.edu>
2025-05-30 16:14:34 -0400

    added some GUI touchups to admin menu, started working towards creating a text file and detecting if username exist, followed by the password. frame now closes and opens up MainFrame on clicking on 'X' button

--- Admin.cpp
+++ Admin.cpp
@@ -7,13 +7,40 @@ Admin::Admin(const wxString& title, wxFrame* frame) : wxFrame(nullptr, wxID_ANY,
 	frame_ = frame;
 
 	wxPanel* loginPanel = new wxPanel(this, wxID_ANY, wxPoint(0, 0), wxSize(800, 800));
-	wxButton* loginButton = new wxButton(loginPanel, wxID_ANY, "Login", wxPoint(100, 500), wxSize(100, 50));
+	wxStaticText* staticUser = new wxStaticText(loginPanel, wxID_ANY, "Enter Username: ", wxPoint(200, 100));
+	wxTextCtrl* usernameInput = new wxTextCtrl(loginPanel, 2, "", wxPoint(300, 100), wxSize(200, 25));
+	wxStaticText* staticPass = new wxStaticText(loginPanel, wxID_ANY, "Enter Password: ", wxPoint(200, 200));
+	wxTextCtrl* passwordInput = new wxTextCtrl(loginPanel, 3, "", wxPoint(300, 200), wxSize(200, 25));
+	wxButton* loginButton = new wxButton(loginPanel, wxID_ANY, "Login", wxPoint(500, 300), wxSize(100, 50));
 	loginButton->Bind(wxEVT_BUTTON, &Admin::loginButtonClicked, this);
+	this->Bind(wxEVT_CLOSE_WINDOW, &Admin::frameOnClose, this);
 
 }
 
 void Admin::loginButtonClicked(wxCommandEvent& evt) {
-	Close();
+	wxDialog* dialog = new wxDialog(this, wxID_ANY, "", wxPoint(500, 300), wxDefaultSize);
+	wxStaticText* staticText = new wxStaticText(dialog, wxID_ANY,
+		"No records exist. \nDo you want to link the information provided to the app?", wxPoint(50, 50), wxDefaultSize);
+	wxButton* button = new wxButton(dialog, wxID_ANY, "Yes", wxPoint(150, 100), wxSize(75, 50));
+
+	std::ifstream database;
+	database.open("Database.txt");
+	if (database.is_open()) {
+		this->Destroy();
+		frame_->Show();
+		frame_ = nullptr;
+	}
+	else {
+		if (dialog->ShowModal() != wxID_OK) {
+			this->Destroy();
+			frame_->Show();
+			frame_ = nullptr;
+		}
+	}
+}
+
+void Admin::frameOnClose(wxCloseEvent& evt) {
+	this->Destroy(); //bad use, but it works. unsure why Close() does not work
 	frame_->Show();
 	frame_ = nullptr;
 }
--- Admin.h
+++ Admin.h
@@ -1,15 +1,15 @@
 #pragma once
-#include "MainFrame.h"
-
-#pragma once
 #include <wx/wx.h>
+#include <fstream>
 
 class Admin : public wxFrame {
 public:
 	Admin(const wxString& title, wxFrame* frame);
 	void loginButtonClicked(wxCommandEvent& evt);
+	void frameOnClose(wxCloseEvent& evt);
 
 
 private:
 	wxFrame* frame_;
+	wxTextCtrl* text_;
 };
